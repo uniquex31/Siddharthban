7f41cc9ccbb13d0f89d6cff3197798e9a6efc56f
Its_UniquE-debug <84559017+UniquE30-debug@users.noreply.github.com>
2023-03-11 16:37:15 +0530

    init

---
+++ script.js
@@ -0,0 +1,95 @@
+/* .ski {
+  width: 100%;
+  margin: 0 auto;
+}
+
+@keyframes load {
+  from {
+      width: 0%
+  }
+}
+
+@-webkit-keyframes load {
+  from {
+      width: 0%
+  }
+}
+
+@-moz-keyframes load {
+  from {
+      width: 0%
+  }
+}
+
+@-o-keyframes load {
+  from {
+      width: 0%
+  }
+}
+
+.bar {
+  background-color: #FFF;
+  border-radius: 15px;
+  margin-bottom: 10px;
+  
+  color: #FFF;
+  font-weight: bold;
+  text-shadow: 1px 1px 1px rgba(255, 255, 255, 0.5);
+}
+
+.bar::before {
+  content: attr(data-skill);
+  background-color: #000000;
+  display: inline-block;
+  padding: 5px 0 5px 10px;
+  border-radius: inherit;
+  animation: load 2s 0s;
+  -webkit-animation: load 2s 0s;
+  -moz-animation: load 2s 0s;
+  -o-animation: load 2s 0s;
+}
+
+.bar.front::before {
+  background-color: #000000;
+}
+
+.bar.back::before {
+  background-color: #000000;
+}
+
+.bar.html::before {
+  width: calc(90% - 10px);
+}
+
+.bar.css::before {
+  width: calc(60% - 10px);
+}
+
+.bar.javascript::before {
+  width: calc(30% - 10px);
+}
+
+.bar.c::before {
+  width: calc(85% - 10px);
+}
+
+.bar.python::before {
+  width: calc(50% - 10px);
+}
+
+.bar.linux::before {
+  width: calc(60% - 10px);
+} 
+<!-- <div class="ski">
+      <span>HTML: 90%</span>
+      <div class=" bar html"></div>
+      <span>CSS: 60%</span>
+      <div class="bar css"></div>
+      <span>JAVASCRIPT: 30%</span>
+      <div class=" bar javascript"></div>
+      <span>C 85%</span>
+      <div class=" bar c"></div>
+      <span>PYTHON: 50%</span>
+      <div class="bar python"></div>
+      <span>LINUX: 60%</span>
+      <div class="bar linux"></div> -->*/
